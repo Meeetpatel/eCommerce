b8652070835ebf9de49a2184559aa64a114e9a52
Meet Patel <meetpatel4567.mp@gmail.com>
2024-02-11 17:05:05 -0500

    m

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./admin.css"
+import "./Admin.css"
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import AddProduct from '../../Components/AddProduct/AddProduct';
